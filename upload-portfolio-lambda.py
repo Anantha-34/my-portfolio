237871320c4a335e31015d12d2d9808cebf2401f
Anantha-34 <ananthu.narayanan@gmail.com>
2017-09-20 23:06:12 +0800

    commit changes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -8,14 +8,29 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:781061244632:deploynoti')
 
+    location = {
+        "bucketName": 'ananthabuild.portfolio.info',
+        "objectKey": 'ananthacodebuild'
+    }
+
     try:
+        job = event.get("CodePipeline.job");
+
+        if job:
+            for artifact in job ["data"] ["inputArtifacts"]:
+                if artifact["name"] == "myAppBuild":
+
+                    location = artifact["location"] ["s3location"]
+
         s3 = boto3.resource('s3',config=Config(signature_version='s3v4'))
 
+        print "Building portfolio from" + str (location)
+
         portfolio_bucket = s3.Bucket ('anantha.portfolio.info');
-        portfoliobuild_bucket = s3.Bucket ('ananthabuild.portfolio.info');
+        portfoliobuild_bucket = s3.Bucket (location["bucketName"]);
 
         portfolio_zip = StringIO.StringIO()
-        portfoliobuild_bucket.download_fileobj('ananthacodebuild',portfolio_zip)
+        portfoliobuild_bucket.download_fileobj(location["objectKey"],portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,6 +41,10 @@ def lambda_handler(event, context):
 
             print "Job done"
             topic.publish(Subject="portfolio deployed",Message="Portfolio deployment was  succecssful")
+
+            if job:
+                codepipeline = boto3.client('codepipeline')
+                codepipeline.put_job_success_result(jobId=job["id"])
 
     except:
         topic.publish(Subject="portfolio deploy failure",Message="Portfolio deployment was not  succecssful")
